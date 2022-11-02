d3350e4c43a775804fd34eab7750ee5e21f8b5ec
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-11-02 11:07:47 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[1])
+
+const primeiraLI = document.querySelector('li');
+console.log(primeiraLI)
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]')
+console.log(linkInterno);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[1]);
