e98890016841ea9f10b35689f17f9af53dd67bb9
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-11-05 14:06:06 -0300

    .

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ img.hasAttribute('id'); // true / false
 img.removeAttribute('alt'); // remove o alt
 */
 
-img.hasAttributes(); // true / false se tem algum atributo
+img.hasAttributes(); // true // false se tem algum atributo
 
 //É muito comum métodos de get e set;
 
@@ -190,7 +190,7 @@ itensMenu.forEach((item )=>{
 itensMenu[0].classList.add('ativo');
 
 // Verifique se as imagens possuem o atributo alt
-const imgs = document.querySelectorAll('img');
+const img14 = document.querySelectorAll('img');
 
 imgs.forEach((img) => {
   const possuiAtibuto = img.hasAttribute('alt');
@@ -200,3 +200,115 @@ imgs.forEach((img) => {
 
 const link = document.querySelectorAll('a[href^="http"]');
 link.setAttribute('href', 'https:www.google.com/');
+
+//HEIGHT E WIDTH
+//Estas são propriedades e métodos dos objetos Element e HTMLElement, a maioria delas são Read Only
+const section = document.querySelector('.animais');
+
+section.clientHeight; // height + padding
+section.offsetHeight; // height + padding + border
+section.scrollHeight; // height total, mesmo dentro de scroll
+
+//Mesma coisa para o Width, clientWidth ...
+
+//OFFSETTOP E OFFSETLEFT
+const section = document.querySelector('.animais');
+
+// Distância entre o topo do elemento e o topo da página
+section.offsetTop;
+
+// Distância entre o canto esquerdo do elemento
+// e o canto esquerdo da página
+section.offsetLeft;
+
+//GETBOUNDINGCLIENTRECT()
+//Método que retorna um objeto com valores de width, height, distâncias do elemento e mais.
+const section = document.querySelector('.animais');
+
+const rect = section.getBoundingClientRect();
+rect.height; // height do elemento
+rect.width; // width do elemento
+rect.top; // distância entre o topo do elemento e o scroll
+
+//WINDOW
+
+window.innerWidth; // width do janela
+window.outerWidth; // soma dev tools também
+window.innerHeight; // height do janela
+window.outerHeight; // soma a barra de endereço
+
+window.pageYOffset; // distância total do scroll vertical
+window.pageXOffset; // distância total do scroll horizontal
+
+if(window.innerWidth < 600) {
+  console.log('Tela menor que 600px');
+}
+
+
+//MATCHMEDIA();
+//Utilize um media-querie como no CSS para verificar a largura do browser
+
+const small = window.matchMedia('(max-width: 600px)');
+
+if(small.matches) {
+  console.log('Tela menor que 600px')
+} else {
+  console.log('Tela maior que 600px')
+}
+
+
+/*DICA
+Selecione o elemento no inspetor (dom)
+
+Abra o console e digite $0 para selecionar o mesmo
+
+Os elementos selecionados anteriormente são $1, $2 ...*/ 
+
+//EXERCÍCIO
+
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const img = document.querySelector('copy');
+const imgTop = img.offsetTop;
+
+console.log(imgTop);
+
+// Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const imagens = document.querySelectorAll('img');
+  let soma = 0;
+  imagens.forEach((imagem) =>{
+    soma = soma + imagem.offsetWidth;
+  });
+
+  console.log(soma);
+}
+
+window.onload = function(){
+  somaImagens();
+}
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const.forEach((link) =>{
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+  if(linkWidth >= 40 && linkHeight >= 48){
+    console.log(link, 'Possui boa acessibilidade')
+  }else(
+    console.log(link, 'Nao possui boa acessibilidade')
+  )
+});
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+const browserSmall = window.matchMedia('(max-width: 720px)').matches;
+
+if(browserSmall){
+  const menu = document.querySelector('.menu');
+  menu.classList.add('menu-mobile');
+}
+
